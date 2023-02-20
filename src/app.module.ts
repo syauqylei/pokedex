1f424baaefc7454ed2f29ad7e75f740e713dbd2b
syauqy <syauqilenterano@gmail.com>
2023-02-21 06:05:37 +0700

    feature: add swagger serve

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ServeStaticModule.],
+  imports: [ServeStaticModule.forRoot({
+    rootPath: join(__dirname,'../src','docs')
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
